fcaacb1a9cabfea5317d2622a4dca91b98ad13cc
zedonarch <zedonix@proton.me>
2026-03-20 13:04:55 +0530

    nix path

--- install.sh
+++ install.sh
@@ -164,7 +164,7 @@ done
 echo "%wheel ALL=(ALL) ALL" >/etc/sudoers.d/wheel
 echo "Defaults pwfeedback" >/etc/sudoers.d/pwfeedback
 echo 'Defaults env_keep += "SYSTEMD_EDITOR XDG_RUNTIME_DIR WAYLAND_DISPLAY DBUS_SESSION_BUS_ADDRESS WAYLAND_SOCKET"' >/etc/sudoers.d/wayland
-echo 'Defaults secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/nix/var/nix/profiles/default/bin"' | sudo tee /etc/sudoers.d/nix-path
+echo 'Defaults secure_path="/nix/var/nix/profiles/default/bin"' | sudo tee /etc/sudoers.d/nix-path
 chmod 440 /etc/sudoers.d/*
 if [[ "$hardware" == "hardware" ]]; then
   usermod -aG libvirt,kvm,lpadmin piyush
