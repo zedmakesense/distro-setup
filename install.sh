9ca00844d1b258f69abafd63e2c29468834883e7
zedonarch <zedonix@proton.me>
2026-03-20 14:17:43 +0530

    ahhh pam

--- install.sh
+++ install.sh
@@ -166,6 +166,9 @@ echo "Defaults pwfeedback" >/etc/sudoers.d/pwfeedback
 echo 'Defaults env_keep += "SYSTEMD_EDITOR XDG_RUNTIME_DIR WAYLAND_DISPLAY DBUS_SESSION_BUS_ADDRESS WAYLAND_SOCKET"' >/etc/sudoers.d/wayland
 echo 'Defaults secure_path="/nix/var/nix/profiles/default/bin:/home/piyush/local/state/nix/profile/bin"' | sudo tee /etc/sudoers.d/nix-path
 chmod 440 /etc/sudoers.d/*
+sed -i '/@include common-auth/a auth optional pam_gnome_keyring.so' /etc/pam.d/login
+sed -i '/pam_keyinit.so/a session optional pam_gnome_keyring.so auto_start' /etc/pam.d/login
+
 if [[ "$hardware" == "hardware" ]]; then
   usermod -aG libvirt,kvm,lpadmin piyush
   chown root:libvirt /var/lib/libvirt/images
