b0e218076a9676e854da30a4af3b2bd44d89a64f
zedonarch <zedonix@proton.me>
2026-03-23 10:10:36 +0530

    no need for that check

--- run.sh
+++ run.sh
@@ -7,13 +7,9 @@ gsettings set org.gnome.desktop.interface gtk-theme 'Gruvbox-Material-Dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
-schema="org.virt-manager.virt-manager.new-vm"
-
-if gsettings list-schemas | grep -qx "$schema"; then
-  gsettings set $schema firmware 'uefi'
-  gsettings set $schema cpu-default 'host-passthrough'
-  gsettings set $schema graphics-type 'spice'
-fi
+gsettings set org.virt-manager.virt-manager.new-vm firmware 'uefi'
+gsettings set org.virt-manager.virt-manager.new-vm cpu-default 'host-passthrough'
+gsettings set org.virt-manager.virt-manager.new-vm graphics-type 'spice'
 
 echo -n "/home/$USER/Documents/projects/default/dotfiles/firefox/ublock.txt" | wl-copy
 dir=$(echo ~/.mozilla/firefox/*.default-esr)
