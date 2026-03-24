c7499c3f94bd473950b00d52110abcd787af09ae
zedonarch <zedonix@proton.me>
2026-03-24 21:50:08 +0530

    sysctl update

--- install.sh
+++ install.sh
@@ -102,16 +102,29 @@ LLMNR=no
 EOF
 
 tee /etc/sysctl.d/99-hardening.conf >/dev/null <<'EOF'
-net.ipv4.conf.all.rp_filter = 1
-net.ipv4.conf.default.rp_filter = 1
 net.ipv4.conf.all.send_redirects = 0
 net.ipv4.conf.default.send_redirects = 0
+net.ipv4.tcp_syncookies = 1
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.default.accept_redirects = 0
+net.ipv4.conf.all.accept_source_route = 0
+net.ipv4.conf.default.accept_source_route = 0
+
+net.ipv6.conf.all.accept_redirects = 0
+net.ipv6.conf.default.accept_redirects = 0
+net.ipv6.conf.all.accept_source_route = 0
+net.ipv6.conf.default.accept_source_route = 0
 
 kernel.kptr_restrict = 2
+kernel.dmesg_restrict = 1
+kernel.unprivileged_bpf_disabled = 1
 
 fs.protected_fifos = 2
+fs.protected_regular = 2
+fs.protected_symlinks = 1
+fs.protected_hardlinks = 1
 
-net.core.bpf_jit_harden = 2
+net.core.bpf_jit_harden = 1
 EOF
 
 sh <(curl -L https://nixos.org/nix/install) --daemon --yes
